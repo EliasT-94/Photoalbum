35253c1f943903bea79620f92133cb000b3877e4
EliasT-94 <elias.tormanen@gmail.com>
2019-06-15 16:55:54 +0300

    Added persist for redux

--- PhotoAlbum/src/reducers/reducers.ts
+++ PhotoAlbum/src/reducers/reducers.ts
@@ -1,43 +1,38 @@
-import {
-    Action,
-    ActionState,
-    ActionType
-  } from "./actions";
-  
-  const initialState = {
-    photo: undefined,
-    album: undefined,
-    photos: [],
-    albums: []
-  };
-  
-  export default function Reducer(
-    state: ActionState = initialState,
-    action: Action
-  ) {
-    switch (action.type) {
-      case ActionType.OPEN_PHOTO:
-        return {
-          ...state,
-          photo: action.payload
-        };
-        case ActionType.OPEN_ALBUM:
-        return {
-          ...state,
-          album: action.payload
-        };
-        case ActionType.GET_PHOTOS:
-        return {
-          ...state,
-          photos: action.payload
-        };
-      case ActionType.GET_ALBUMS:
-        console.log(action.payload);
-        return {
-          ...state,
-          albums: action.payload
-        };        default: 
-        return state
-    }
+import { Action, ActionState, ActionType } from "./actions";
+
+const initialState = {
+  photo: undefined,
+  album: undefined,
+  photos: [],
+  albums: []
+};
+
+export default function Reducer(
+  state: ActionState = initialState,
+  action: Action
+) {
+  switch (action.type) {
+    case ActionType.OPEN_PHOTO:
+      return {
+        ...state,
+        photo: action.payload
+      };
+    case ActionType.OPEN_ALBUM:
+      return {
+        ...state,
+        album: action.payload
+      };
+    case ActionType.GET_PHOTOS:
+      return {
+        ...state,
+        photos: action.payload
+      };
+    case ActionType.GET_ALBUMS:
+      return {
+        ...state,
+        albums: action.payload
+      };
+    default:
+      return state;
   }
-  
+}
--- PhotoAlbum/store.ts
+++ PhotoAlbum/store.ts
@@ -1,7 +1,19 @@
 import { createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import rootReducer from "./src/reducers";
+import storage from "redux-persist/lib/storage";
 
-import reducers from "./src/reducers/";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 
-const store = createStore(reducers);
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  stateReconciler: autoMergeLevel2,
+  whitelist: ["albums", "photos"],
+  blacklist: ["album", "photo"]
+};
+const pReducer = persistReducer(persistConfig, rootReducer);
 
+const store = createStore(pReducer);
+export const persistor = persistStore(store);
 export default store;
